a506d0a50f84081322a12e493bc281f7830258c8
abtorres <abtorres079@gmail.com>
2019-07-02 00:26:59 -0600

    navbar is styled using css

--- front_end/tshirts/src/components/landingpage/Nav.js
+++ front_end/tshirts/src/components/landingpage/Nav.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import './Nav.css';
 
 // const Div = styled.div`
 //     display: flex;
@@ -8,11 +9,14 @@ import { faUser, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 
 function Nav() {
     return (
-        <div>
-            <p>Logo</p>
-            <div> <FontAwesomeIcon icon={faShoppingCart} /> </div>  
-            <div> <FontAwesomeIcon icon={faUser} /> </div>  
-
+        <div className='navi'>
+            <div className='logos'>
+                <p className='logo'>Logo</p>
+            </div>
+            <div className='icons'>
+                <div className='icon'> <FontAwesomeIcon icon={faUser} /> 
+                </div><div className='icon'> <FontAwesomeIcon icon={faShoppingCart} /> </div>   
+            </div>
         </div>
     );
 }
